1e0c2867324e9255455ecc03086ec410a9e54d64
root <niso120b@gmail.com>
2016-09-27 10:13:05 +0300

    Fix kubelet api and add the commands

--- openstack/instanceboot.sh
+++ openstack/instanceboot.sh
@@ -51,6 +51,7 @@ function kubernetes_single_node() {
     apt-get update && apt-get install git -y
     cd /opt/install && git clone https://github.com/niso120b/Kubernetes-Single-Node.git .
     cd /opt/install/scripts && ./deploy-local-cluster.sh
+    mv /opt/bin/* /bin/
   else
     echo "*** Error to deploy a kubernetes on single node server /etc/install exists" && exit 1
   fi
